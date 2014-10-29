6cf3ff215833d2ba26042608eb76c4eedc9a51d6
Mickaël Salaün <mickael.salaun@ssi.gouv.fr>
2014-10-29 18:27:11 +0100

    ffi/gen: Replace "static" with "const"

--- src/ffi/gen/fs.rs
+++ src/ffi/gen/fs.rs
@@ -7,84 +7,84 @@ use self::libc::c_ulong;
 bitflags!(
     flags MsFlags: c_ulong {
         /* Mount read-only */
-        static MsRdonly = 1,
+        const MsRdonly = 1,
 
         /* Ignore suid and sgid bits */
-        static MsNosuid = 2,
+        const MsNosuid = 2,
 
         /* Disallow access to device special files */
-        static MsNodev = 4,
+        const MsNodev = 4,
 
         /* Disallow program execution */
-        static MsNoexec = 8,
+        const MsNoexec = 8,
 
         /* Writes are synced at once */
-        static MsSynchronous = 16,
+        const MsSynchronous = 16,
 
         /* Alter flags of a mounted FS */
-        static MsRemount = 32,
+        const MsRemount = 32,
 
         /* Allow mandatory locks on an FS */
-        static MsMandlock = 64,
+        const MsMandlock = 64,
 
         /* Directory modifications are synchronous */
-        static MsDirsync = 128,
+        const MsDirsync = 128,
 
         /* Do not update access times. */
-        static MsNoatime = 1024,
+        const MsNoatime = 1024,
 
         /* Do not update directory access times */
-        static MsNodiratime = 2048,
+        const MsNodiratime = 2048,
 
-        static MsBind = 4096,
+        const MsBind = 4096,
 
-        static MsMove = 8192,
+        const MsMove = 8192,
 
-        static MsRec = 16384,
+        const MsRec = 16384,
 
-        static MsVerbose = 32768,
+        const MsVerbose = 32768,
 
-        static MsSilent = 32768,
+        const MsSilent = 32768,
 
         /* VFS does not apply the umask */
-        static MsPosixacl = (1<<16),
+        const MsPosixacl = (1<<16),
 
         /* change to unbindable */
-        static MsUnbindable = (1<<17),
+        const MsUnbindable = (1<<17),
 
         /* change to private */
-        static MsPrivate = (1<<18),
+        const MsPrivate = (1<<18),
 
         /* change to slave */
-        static MsSlave = (1<<19),
+        const MsSlave = (1<<19),
 
         /* change to shared */
-        static MsShared = (1<<20),
+        const MsShared = (1<<20),
 
         /* Update atime relative to mtime/ctime. */
-        static MsRelatime = (1<<21),
+        const MsRelatime = (1<<21),
 
         /* this is a kern_mount call */
-        static MsKernmount = (1<<22),
+        const MsKernmount = (1<<22),
 
         /* Update inode I_version field */
-        static MsIVersion = (1<<23),
+        const MsIVersion = (1<<23),
 
         /* Always perform atime updates */
-        static MsStrictatime = (1<<24),
+        const MsStrictatime = (1<<24),
 
-        static MsNosec = (1<<28),
+        const MsNosec = (1<<28),
 
-        static MsBorn = (1<<29),
+        const MsBorn = (1<<29),
 
-        static MsActive = (1<<30),
+        const MsActive = (1<<30),
 
-        static MsNouser = (1<<31),
+        const MsNouser = (1<<31),
 
-        static MsRmtMask = (MsRdonly.bits|MsSynchronous.bits|MsMandlock.bits|MsIVersion.bits),
+        const MsRmtMask = (MsRdonly.bits|MsSynchronous.bits|MsMandlock.bits|MsIVersion.bits),
 
-        static MsMgcVal = 0xC0ED0000,
+        const MsMgcVal = 0xC0ED0000,
 
-        static MsMgcMsk = 0xffff0000
+        const MsMgcMsk = 0xffff0000
     }
 )
--- src/ffi/gen/fs0.rs
+++ src/ffi/gen/fs0.rs
@@ -7,18 +7,18 @@ use self::libc::c_uint;
 bitflags!(
     flags MntFlags: c_uint {
         /* Attempt to forcibily umount */
-        static MntForce = 0x00000001,
+        const MntForce = 0x00000001,
 
         /* Just detach from the tree */
-        static MntDetach = 0x00000002,
+        const MntDetach = 0x00000002,
 
         /* Mark for expiry */
-        static MntExpire = 0x00000004,
+        const MntExpire = 0x00000004,
 
         /* Don't follow symlink on umount */
-        static UmountNofollow = 0x00000008,
+        const UmountNofollow = 0x00000008,
 
         /* Flag guaranteed to be unused */
-        static UmountUnused = 0x80000000
+        const UmountUnused = 0x80000000
     }
 )
