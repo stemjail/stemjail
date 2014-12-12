039b7f0befbcd2c04904e3641c3104afbb4e5782
Mickaël Salaün <mickael.salaun@ssi.gouv.fr>
2014-12-12 18:04:30 +0100

    tools/kage: Fix profile name and add more debug

--- tools/kage.sh
+++ tools/kage.sh
@@ -2,9 +2,9 @@
 
 DIR_BASE="$(readlink -f -- "$(dirname -- "$0")/..")"
 
-ARGS="run foo -t sh"
+ARGS="run example1 -t sh"
 if [ $# -ne 0 ]; then
 	ARGS="$*"
 fi
 
-LD_LIBRARY_PATH="${DIR_BASE}/target/deps" RUST_LOG=stemjail=debug "${DIR_BASE}/target/kage" ${ARGS}
+LD_LIBRARY_PATH="${DIR_BASE}/target/deps" RUST_LOG=stemjail=debug,kage=debug "${DIR_BASE}/target/kage" ${ARGS}
