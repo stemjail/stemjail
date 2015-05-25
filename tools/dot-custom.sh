265971d7b5cbd6dae539b4afec99d28541e841f8
Mickaël Salaün <mic@digikod.net>
2015-05-25 18:38:56 +0200

    tools/dot-custom: Add script to color graph

---
+++ tools/dot-custom.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Set domain and access colors
+
+sed -r '
+	s/(^ +D_[0-9]+\[[^]]+)(];)$/\1, style="filled", fillcolor="#b3e1ff"\2/;
+	s/(^ +A_[0-9]+\[[^]]+)(];)$/\1, style="filled", fillcolor="#97f1ae"\2/;
+	/home/ s/( fillcolor=")#[a-f0-9]+/\1#0a9c30/;
+	'
