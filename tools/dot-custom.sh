6edf63fe704310f5e36b00ebf872385592fee6e9
Mickaël Salaün <mic@digikod.net>
2015-05-25 19:58:15 +0200

    tools/dot-custom: Change shape for intersection domains

--- tools/dot-custom.sh
+++ tools/dot-custom.sh
@@ -6,4 +6,5 @@ sed -r '
 	s/(^ +D_[0-9]+\[[^]]+)(];)$/\1, style="filled", fillcolor="#b3e1ff"\2/;
 	s/(^ +A_[0-9]+\[[^]]+)(];)$/\1, style="filled", fillcolor="#97f1ae"\2/;
 	/home/ s/( fillcolor=")#[a-f0-9]+/\1#0a9c30/;
+	/ &cap; / s/( style=)/ shape=box,\1/;
 	'
