0c3f0771c42530507387f5c41daaa40fe798121c
Mickaël Salaün <mic@digikod.net>
2015-06-01 22:05:44 +0200

    tools/dom-custom: Change colors and shapes for better contrast

--- tools/dot-custom.sh
+++ tools/dot-custom.sh
@@ -3,8 +3,8 @@
 # Set domain and access colors
 
 sed -r '
-	s/(^ +D_[0-9]+\[[^]]+)(];)$/\1, style="filled", fillcolor="#b3e1ff"\2/;
-	s/(^ +A_[0-9]+\[[^]]+)(];)$/\1, style="filled", fillcolor="#97f1ae"\2/;
-	/home/ s/( fillcolor=")#[a-f0-9]+/\1#0a9c30/;
-	/ &cap; / s/( style=)/ shape=box,\1/;
+	s/(^ +D_[0-9]+\[[^]]+)(];)$/\1, style="filled", fillcolor="#64c3ff"\2/;
+	s/(^ +A_[0-9]+\[[^]]+)(];)$/\1, style="filled", fillcolor="#97f1ae", shape=box\2/;
+	/home/ s/( fillcolor=")#[a-f0-9]+/\1#00e73c/;
+	/ &cap; / s/( fillcolor=")#[a-f0-9]+/\1#b3e1ff/;
 	'
