239ed47979b77c3423548adf0ad52d6ab03c1ab3
Mickaël Salaün <mickael.salaun@ssi.gouv.fr>
2015-11-13 15:24:24 +0100

    cmd/shim: Fix release mode
    Using BufStream with UnixStream cause the recv() being executed before
    the send(), which result in a deadlock.
    
    Do not use BufStream!

--- src/cmd/shim/fsm_kage.rs
+++ src/cmd/shim/fsm_kage.rs
@@ -14,7 +14,6 @@
 
 /// Finite-state machine for a `KageCommand` call
 
-use bufstream::BufStream;
 use cmd::MonitorCall;
 use cmd::util::{recv, send};
 use MONITOR_SOCKET_PATH;
@@ -25,7 +24,7 @@ use unix_socket::UnixStream;
 macro_rules! fsm_next {
     ($myself: expr) => {
         KageFsm {
-            bstream: $myself.bstream,
+            stream: $myself.stream,
             _state: PhantomData,
         }
     }
@@ -43,18 +42,18 @@ mod state {
 }
 
 pub struct KageFsm<T> {
-    bstream: BufStream<UnixStream>,
+    stream: UnixStream,
     _state: PhantomData<T>,
 }
 
 impl KageFsm<state::Init> {
     pub fn new() -> Result<KageFsm<state::Init>, String> {
-        let bstream = match UnixStream::connect(MONITOR_SOCKET_PATH) {
-            Ok(s) => BufStream::new(s),
+        let stream = match UnixStream::connect(MONITOR_SOCKET_PATH) {
+            Ok(s) => s,
             Err(e) => return Err(format!("Failed to connect: {}", e)),
         };
         Ok(KageFsm {
-            bstream: bstream,
+            stream: stream,
             _state: PhantomData,
         })
     }
@@ -62,26 +61,26 @@ impl KageFsm<state::Init> {
     pub fn send_list_request(mut self, req: ListRequest)
             -> Result<KageFsm<state::RecvList>, String> {
         let action = MonitorCall::Shim(ShimAction::List(req));
-        try!(send(&mut self.bstream, action));
+        try!(send(&mut self.stream, action));
         Ok(fsm_next!(self))
     }
 
     pub fn send_access_request(mut self, req: AccessRequest)
             -> Result<KageFsm<state::RecvAcl>, String> {
         let action = MonitorCall::Shim(ShimAction::Access(req));
-        try!(send(&mut self.bstream, action));
+        try!(send(&mut self.stream, action));
         Ok(fsm_next!(self))
     }
 }
 
 impl KageFsm<state::RecvList> {
     pub fn recv_list_response(mut self) -> Result<ListResponse, String> {
-        recv(&mut self.bstream)
+        recv(&mut self.stream)
     }
 }
 
 impl KageFsm<state::RecvAcl> {
     pub fn recv_access_response(mut self) -> Result<AccessResponse, String> {
-        recv(&mut self.bstream)
+        recv(&mut self.stream)
     }
 }
