35d3268708a9a195d5bf9cfd14c778128daad325
Mickaël Salaün <mickael.salaun@ssi.gouv.fr>
2015-11-13 18:46:29 +0100

    tools/kage: Use the preloaded library by default

--- tools/kage.sh
+++ tools/kage.sh
@@ -3,9 +3,14 @@
 RUSTC_MODE="${RUSTC_MODE:-release}"
 DIR_BASE="$(dirname -- "$(readlink -f -- "$0")")/.."
 
-ARGS="run -t -- /usr/bin/setsid -c /bin/bash"
+ARGS="run -t -- ${DIR_BASE}/tools/env.sh /usr/bin/setsid -c /bin/bash"
 if [ $# -ne 0 ]; then
 	ARGS="$*"
 fi
 
-LD_LIBRARY_PATH="${DIR_BASE}/target/${RUSTC_MODE}/deps" RUST_LOG=stemjail=debug,kage=debug "${DIR_BASE}/target/${RUSTC_MODE}/kage" ${ARGS}
+LIB="${DIR_BASE}/target/${RUSTC_MODE}/deps"
+if [ -n "${LD_LIBRARY_PATH}" ]; then
+	LIB="${LD_LIBRARY_PATH}:${LIB}"
+fi
+
+LD_LIBRARY_PATH="${LIB}" RUST_LOG=stemjail=debug,kage=debug "${DIR_BASE}/target/${RUSTC_MODE}/kage" ${ARGS}
