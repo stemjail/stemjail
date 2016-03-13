ed154139da8b4e8c1a3ea909d39037007d89c08c
Mickaël Salaün <mic@digikod.net>
2016-03-13 14:45:11 +0100

    tools/{kage,portal}: Set current directory to the base one

--- tools/portal.sh
+++ tools/portal.sh
@@ -2,5 +2,6 @@
 
 RUSTC_MODE="${RUSTC_MODE:-release}"
 DIR_BASE="$(dirname -- "$(readlink -f -- "$0")")/.."
+cd "${DIR_BASE}"
 
 LD_LIBRARY_PATH="${DIR_BASE}/target/${RUSTC_MODE}/deps" RUST_LOG=stemjail=debug,portal=debug "${DIR_BASE}/target/${RUSTC_MODE}/portal" "$@"
